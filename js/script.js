c84859bf62ebde88990f2c8a4879ca9fcca4dcf3
Vaibhav <58388765+VebzDeveloper@users.noreply.github.com>
2025-04-11 16:42:03 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// var typed = new Typed('#element', {
+//     strings: ['Full Stack Developer'],
+//     typeSpeed: 50, // Speed of typing
+//     backSpeed: 30, // Speed of backspacing
+//     startDelay: 500, // Delay before typing starts
+//     backDelay: 1000, // Delay before backspacing
+//     loop: true, // Loop the animation
+//     smartBackspace: true, // Only backspace what's necessary
+//     showCursor: true, // Show blinking cursor
+//     cursorChar: '|', // Customize cursor character
+// });
+var typed = new Typed('#element', {
+    strings: ['Full Stack Developer', 'Web Developer', 'Front End Developer', 'Back End Developer'],
+    typeSpeed: 50, // Speed of typing
+    backSpeed: 30, // Speed of backspacing
+    startDelay: 500, // Delay before typing starts
+    backDelay: 1000, // Delay before backspacing
+    loop: true, // Loop the animation
+    smartBackspace: true, // Only backspace what's necessary
+    showCursor: true, // Show blinking cursor
+    cursorChar: '|', // Customize cursor character
+});
